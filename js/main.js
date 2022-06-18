57eb64386679b33f5e06881ea9ba0baaaff2ce68
chaemin-00515 <w2114@e-mirim.hs.kr>
2022-06-19 02:30:43 +0900

    main 좋아요 버튼 구현

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let cnt = 0;
+function addLike() {
+    const button = document.getElementById("heart");
+    const pushHeartBtn = document.querySelector(".heartBtn");
+    cnt += 1;
+    if (cnt % 2 == 0) {
+        pushHeartBtn.innerHTML = '<i class="bx bx-heart" ></i>';
+        pushHeartBtn.style.color = 'black';
+    } else {
+        pushHeartBtn.innerHTML = '<i class="bx bxs-heart" ></i>';
+        pushHeartBtn.style.color = 'red';
+    }
+}
