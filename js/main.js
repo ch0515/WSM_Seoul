926544be55e4441807c87715451f529c49953dfc
chaemin-00515 <w2114@e-mirim.hs.kr>
2022-06-20 20:37:53 +0900

    heart 추가 태그 추가

--- js/main.js
+++ js/main.js
@@ -11,7 +11,102 @@ function addLike() {
         pushHeartBtn.style.color = 'red';
     }
 }
-// function addbtn(){
-//     const button = document.getElementById("btn1");
-//     alert("태그 추가 완료!");
-// }
+function addbtn(){
+    const button = document.getElementById("btn1");
+    alert("태그 추가 완료!");
+}
+function addLike1() {
+    const button = document.getElementById("heart1");
+    const pushHeartBtn = document.querySelector(".heartBtn1");
+    cnt += 1;
+    if (cnt % 2 == 0) {
+        pushHeartBtn.innerHTML = '<i class="bx bx-heart" ></i>';
+        pushHeartBtn.style.color = 'black';
+    } else {
+        pushHeartBtn.innerHTML = '<i class="bx bxs-heart" ></i>';
+        pushHeartBtn.style.color = 'red';
+    }
+}
+function addLike2() {
+    const button = document.getElementById("heart2");
+    const pushHeartBtn = document.querySelector(".heartBtn2");
+    cnt += 1;
+    if (cnt % 2 == 0) {
+        pushHeartBtn.innerHTML = '<i class="bx bx-heart" ></i>';
+        pushHeartBtn.style.color = 'black';
+    } else {
+        pushHeartBtn.innerHTML = '<i class="bx bxs-heart" ></i>';
+        pushHeartBtn.style.color = 'red';
+    }
+}
+function addLike3() {
+    const button = document.getElementById("heart3");
+    const pushHeartBtn = document.querySelector(".heartBtn3");
+    cnt += 1;
+    if (cnt % 2 == 0) {
+        pushHeartBtn.innerHTML = '<i class="bx bx-heart" ></i>';
+        pushHeartBtn.style.color = 'black';
+    } else {
+        pushHeartBtn.innerHTML = '<i class="bx bxs-heart" ></i>';
+        pushHeartBtn.style.color = 'red';
+    }
+}
+function addLike4() {
+    const button = document.getElementById("heart4");
+    const pushHeartBtn = document.querySelector(".heartBtn4");
+    cnt += 1;
+    if (cnt % 2 == 0) {
+        pushHeartBtn.innerHTML = '<i class="bx bx-heart" ></i>';
+        pushHeartBtn.style.color = 'black';
+    } else {
+        pushHeartBtn.innerHTML = '<i class="bx bxs-heart" ></i>';
+        pushHeartBtn.style.color = 'red';
+    }
+}
+function addLike5() {
+    const button = document.getElementById("heart5");
+    const pushHeartBtn = document.querySelector(".heartBtn5");
+    cnt += 1;
+    if (cnt % 2 == 0) {
+        pushHeartBtn.innerHTML = '<i class="bx bx-heart" ></i>';
+        pushHeartBtn.style.color = 'black';
+    } else {
+        pushHeartBtn.innerHTML = '<i class="bx bxs-heart" ></i>';
+        pushHeartBtn.style.color = 'red';
+    }
+}
+function addLike6() {
+    const button = document.getElementById("heart6");
+    const pushHeartBtn = document.querySelector(".heartBtn6");
+    cnt += 1;
+    if (cnt % 2 == 0) {
+        pushHeartBtn.innerHTML = '<i class="bx bx-heart" ></i>';
+        pushHeartBtn.style.color = 'black';
+    } else {
+        pushHeartBtn.innerHTML = '<i class="bx bxs-heart" ></i>';
+        pushHeartBtn.style.color = 'red';
+    }
+}
+function addLike7() {
+    const button = document.getElementById("heart7");
+    const pushHeartBtn = document.querySelector(".heartBtn7");
+    cnt += 1;
+    if (cnt % 2 == 0) {
+        pushHeartBtn.innerHTML = '<i class="bx bx-heart" ></i>';
+        pushHeartBtn.style.color = 'black';
+    } else {
+        pushHeartBtn.innerHTML = '<i class="bx bxs-heart" ></i>';
+        pushHeartBtn.style.color = 'red';
+    }
+}function addLike8() {
+    const button = document.getElementById("heart8");
+    const pushHeartBtn = document.querySelector(".heartBtn8");
+    cnt += 1;
+    if (cnt % 2 == 0) {
+        pushHeartBtn.innerHTML = '<i class="bx bx-heart" ></i>';
+        pushHeartBtn.style.color = 'black';
+    } else {
+        pushHeartBtn.innerHTML = '<i class="bx bxs-heart" ></i>';
+        pushHeartBtn.style.color = 'red';
+    }
+}
